2a2b7143df5fb2b4454ed6f45cd47e2c819c43c5
Luis Capelo <luis@gridai.co>
2021-05-12 21:48:46 -0400

    adds comment

--- fdetection/data.py
+++ fdetection/data.py
@@ -27,6 +27,8 @@ class DataProcessing:
 
         # creates image cropping pipeling; use GPU if available
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # default optiosn lifted from https://github.com/timesler/facenet-pytorch/blob/master/examples/infer.ipynb
         self.mtcnn = MTCNN(
             image_size=160, margin=0, min_face_size=20,
             thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
